90ddb4559d1a2112225cbe37970c9386e0f628ce
fpestunov <fpestunov@gmail.com>
2019-07-16 20:51:55 +0400

    add solution for 9 and 100

--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -5,16 +5,17 @@ class PrimeFactors
 	public function generate($number)
 	{
 		$primes = [];
+		$candidate = 2;
 
-		while ($number % 2 === 0) {
-			$primes[] = 2;
-			$number /= 2;
+		while ($number > 1) {
+			while ($number % $candidate === 0) {
+				$primes[] = $candidate;
+				$number /= $candidate;
+			}
+			
+			$candidate++;
 		}
 		
-		if ($number > 1) {
-			$primes[] = $number;
-		}
-
 		return $primes;
 	}
 }
