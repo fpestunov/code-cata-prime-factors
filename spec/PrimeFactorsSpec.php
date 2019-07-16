5f2f6f85e9fe3cea6ef479b01b96b7c6cea80c0d
fpestunov <fpestunov@gmail.com>
2019-07-16 18:33:19 +0400

    init project

---
+++ spec/PrimeFactorsSpec.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace spec;
+
+use PrimeFactors;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class PrimeFactorsSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(PrimeFactors::class);
+    }
+}
